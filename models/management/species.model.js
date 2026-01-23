87ac58d6e71417fde181d8153207862dc3843a8c
glitched0xff <mauro.prandelli@gmail.com>
2026-01-23 19:13:21 +0100

    update species model

--- models/management/species.model.js
+++ models/management/species.model.js
@@ -2,20 +2,15 @@ const DataTypes= require("sequelize")
 
 module.exports = (sequelize, Sequelize) => {
     const Species = sequelize.define("species", {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
+        id: {type: Sequelize.INTEGER,primaryKey: true,autoIncrement: true
           },
-          species_name:{
-            type: Sequelize.STRING
-          },
-          description:{
-            type: Sequelize.STRING
-          },
-          shortCode:{
-            type: Sequelize.STRING
-          },
+        species_name:{type: Sequelize.STRING},
+        description:{type: Sequelize.STRING},
+        shortCode:{type: Sequelize.STRING},
+        synonyms:{type: Sequelize.STRING},
+        habitat:{type: Sequelize.STRING},
+        substrate:{type: Sequelize.STRING},
+        longDesc:{type: Sequelize.STRING},
           
     });
     Species.associate = (models) => {
