6dee151ab008c862723ad8676e704c0a8e33f86b
glitched0xff <mauro.prandelli@gmail.com>
2026-03-03 16:28:41 +0100

    Update

--- routes/management/mushElement.js
+++ routes/management/mushElement.js
@@ -770,7 +770,7 @@ router.get('/csvLotto',async (req,res)=>{
         let elem=mushElements[i]
         csvObj.push({
             element_code:elem.element_code,
-            createAt:moment( elem.createLot).format("DD-MM-YY HH:mm"),
+            createAt:moment( elem.load_date).format("DD-MM-YY HH:mm"),
             qrCode:"/mushElement/mushElementLanding?elementCode="+elem.element_code
         })
     }
